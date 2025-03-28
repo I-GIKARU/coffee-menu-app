fe75c4f213effca951670835e64b16bf531fcee9
User <user@example.com>
2025-03-28 09:16:21 +0300

    22nd commit added render url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // API Configuration on render 
-const API_ENDPOINT = 'http://localhost:3000/';
+const API_ENDPOINT = 'https://coffee-db.onrender.com';
 
 // DOM Elements declared as constants
 const coffeeList = document.getElementById('coffee-list');
